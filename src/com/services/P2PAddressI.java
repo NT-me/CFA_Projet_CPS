135a850e41b1d749b6d35609cc8900d1d47a2761
Hon4m1 <silver-bird@hotmail.fr>
2021-07-19 15:06:10 +0200

    Interfaces comments updated

--- src/com/services/P2PAddressI.java
+++ src/com/services/P2PAddressI.java
@@ -2,8 +2,19 @@ package com.services;
 
 public interface P2PAddressI extends AddressI {
 
-    boolean isP2PAddress();
-
-    boolean isIPAddress();
+    /**
+     * Description : Ensure that this address is a P2PAddress
+     * @return true if P2PAddress, otherwise false
+     */
+    default boolean isP2PAddress() {
+        return true;
+    }
+    /**
+     * Description : Ensure that this address is a IPPAddress
+     * @return true if IPAddress, otherwise false
+     */
+    default boolean isIPAddress() {
+        return false;
+    }
 
 }
--- src/com/services/RegistrationCI.java
+++ src/com/services/RegistrationCI.java
@@ -9,29 +9,29 @@ import java.util.Set;
 public interface RegistrationCI extends OfferedCI, RequiredCI {
 
 
-    /**
-     *  Description :
+    /** TODO remplir la javadoc
+     * Description :
+     * @param address of type P2PAddressI - The P2PAddress of the device
+     * @param communicationInboundPortURI of type String - The URI of the communication entry port of the device
+     * @param initialPositionI of type PositionI - The position inside the simulator of the device
+     * @param initialRange of type double - The range of the device
+     * @param routingInboundPortURI of type
+     * @return a Set of ConnectionInformations containing informations about the neighbours
+     * @throws Exception in case of errors
+     */
+    Set<ConnectionInfo> registerInternal(P2PAddressI address, String communicationInboundPortURI, PositionI initialPositionI, double initialRange,String routingInboundPortURI ) throws Exception;
+
+    /** TODO
+     * Description :
      * @param address of type P2PAddressI - The P2PAddress of the device
      * @param communicationInboundPortURI of type String - The URI of the communication entry port of the device
      * @param initialPositionI of type PositionI - The position inside the simulator of the device
      * @param initialRange of type double - The range of the device
      * @param routingInboundPortURI
-     * @return a Set of ConnectionInformations
-     * @throws Exception
+     * @return a set of type ConnectionInfo - List of information about all
+     * @throws Exception in case of error
      */
-    public Set<ConnectionInfo> registerInternal(P2PAddressI address, String communicationInboundPortURI, PositionI initialPositionI, double initialRange,String routingInboundPortURI ) throws Exception;
+    Set<ConnectionInfo> registerAccessPoint(P2PAddressI address, String communicationInboundPortURI, PositionI initialPositionI, double initialRange,String routingInboundPortURI ) throws Exception;
 
-    /**
-     *  Description :
-     * @param address of type P2PAddressI - The P2PAddress of the device
-     * @param communicationInboundPortURI of type String - The URI of the communication entry port of the device
-     * @param initialPositionI of type PositionI - The position inside the simulator of the device
-     * @param initialRange of type double - The range of the device
-     * @param routingInboundPortURI
-     * @return
-     * @throws Exception
-     */
-    public Set<ConnectionInfo> registerAccessPoint(P2PAddressI address, String communicationInboundPortURI, PositionI initialPositionI, double initialRange,String routingInboundPortURI ) throws Exception;
-
-    //public void unregister(P2PAddressI address);
+    //void unregister(P2PAddressI address);
 }
